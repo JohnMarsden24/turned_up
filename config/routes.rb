36564316ec5c2150d93a028e1c044fad4dc02623
Ben Sargeant <ben.stroodcp@gmail.com>
2020-02-24 17:54:44 +0000

    added the bookings routes nested in the user routes where required

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  resources :users do
+    resources :bookings, except: [:index, :show, :destroy]
+  end
+
+  resources :bookings, only: [:destroy]
+
 end
