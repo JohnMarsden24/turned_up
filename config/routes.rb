37283919b731cc3260306832248e3cb5be89c4dc
Ben Sargeant <ben.stroodcp@gmail.com>
2020-02-24 18:52:25 +0000

    moved the edit and update of reviews and bookings outside of the nested routes as we only need the booking/review ID to edit/update it it - the same as the destory method

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users do
-    resources :bookings, except: [:index, :show, :destroy]
+    resources :bookings, except: [:index, :show, :destroy, :edit, :update]
     resources :reviews, except: [:index, :show, :destroy]
   end
 
-  resources :bookings, only: [:destroy]
-  resources :reviews, only: [:destroy]
+  resources :bookings, only: [:destroy, :edit, :update]
+  resources :reviews, only: [:destroy,  :edit, :update]
 
 end
