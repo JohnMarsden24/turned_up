80f96ee4207add8292bd669f8761020a57197254
Ben Sargeant <ben.stroodcp@gmail.com>
2020-02-24 17:58:35 +0000

    skeleton controllers completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users do
     resources :bookings, except: [:index, :show, :destroy]
+    resources :reviews, except: [:index, :show, :destroy]
   end
 
   resources :bookings, only: [:destroy]
+  resources :reviews, only: [:destroy]
 
 end
