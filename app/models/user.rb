43493d97aea67181d1f9a94ea5d0e597e7efac60
John Marsden <john.marsden24@gmail.com>
2020-02-24 16:39:43 +0000

    n:n between user(artist) and user(booker)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  has_many :reviews, class_name: "Review", foreign_key: "user_id"
+  has_many :reviewed, class_name: "Review", foreign_key: "artist_id"
+
+  has_many :bookings, class_name: "Booking", foreign_key: "user_id"
+  has_many :booked, class_name: "Booking", foreign_key: "artist_id"
+
+  # validates
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
