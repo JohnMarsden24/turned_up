06c973f4a34e29bd6e6110cea94ce3eb6538ae83
Jan Moelich <janmoelich@gmail.com>
2020-02-25 12:28:08 +0000

    Created a new & create method for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,20 @@
 class BookingsController < ApplicationController
 
   def new
+    @booking = Booking.new
+    @artist = User.find(params[:user_id])
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.artist = User.find(params[:user_id])
+    raise
+    if @booking.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -13,6 +24,12 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :date)
   end
 
 end
